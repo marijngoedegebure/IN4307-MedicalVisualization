9893d7788317cda0de255124fad3d0bde8bc5968
Maikel Langezaal <maikellangezaal@hotmail.com>
2015-12-18 23:04:08 +0100

    Kmeans mostly done
    Needs some cleanup and interaction with the button

--- project/scatterplot.js
+++ project/scatterplot.js
@@ -1,6 +1,14 @@
 var viewWidth = 500;
 var viewHeight = 500;
 
+var clusterNum = 10;
+var clusterColor = ['#a00', '#aa0', '#0a0', '#00a', '#0aa','#7fffd4', '#8b2323', '#ff7f24', '#ff1493', '#c0ff3e'];
+var centroidColor = ['#f00', '#ff0', '#0f0', '#00f', '#0ff','#7fffd4', '#8b2323', '#ff7f24', '#ff1493', '#c0ff3e'];
+var clusterC = [];
+
+var finish = false;
+var centroidArr = [];
+
 var margin = {top: 20, right: 20, bottom: 30, left: 40};
 var width = viewWidth - margin.left - margin.right;
 var height = viewHeight - margin.top - margin.bottom;
@@ -32,9 +40,18 @@ var points;
 
 var tooltip = d3.select('.tooltip-content');
 
+var data;
+
+var xFunc;
+var yFunc;
+
 function drawScatterplot() {
 	
-  var data = tsne_data.nodes;
+  data = tsne_data.nodes;
+  
+  for (var i = 0; i < data.length; i++) {
+	  clusterC[i] = parseInt(Math.random() * clusterNum, 10) + 1
+  }
   
   var xExtent = d3.extent(data, function(d) { return d.tsneX; });
   var yExtent = d3.extent(data, function(d) { return d.tsneY; });
@@ -45,7 +62,10 @@ function drawScatterplot() {
   color.domain(zExtent);
 
   svg.selectAll("g").remove();
-
+  
+  xFunc = x;
+  yFunc = y;
+  
   points = svg.append("g")
       .attr("class", "plotArea")
     .selectAll(".dot")
@@ -65,30 +85,114 @@ function drawScatterplot() {
                .duration(500);
           tooltip.html("");
     });
+	
+	drawClusters();
+	calCentroid();
 }
 
-function updatePoints(v1, v2 ,v3) {
+function drawClusters() {
+	d3.selectAll('.dot')
+        .data(data)
+        .attr({
+            fill : 
+			function(d,i){
+				return clusterColor[clusterC[i]]
+			;}
+    })
+}
 
-  var xExtent = d3.extent(tsne_data.nodes, function(d) { return d[v1]; });
-  var yExtent = d3.extent(tsne_data.nodes, function(d) { return d[v2]; });
-  var zExtent = d3.extent(tsne_data.nodes, function(d) { return d[v3]; });
 
-  x.domain(xExtent).nice();
-  y.domain(yExtent).nice();
-  color.domain(zExtent);
+function calCentroid () {
+    var dataset = d3.selectAll('.dot')[0];    
+    var notMove = 0;
+    for (var j = 1; j <= clusterNum; j++) {
+        var c = {
+            "x": d3.mean(dataset, function (d,i) {				
+                x = d.getAttribute('cx');
+                cluster = clusterC[i];
+                
+                if (cluster == j) {
+                    return parseInt(x, 10);
+                }
+            }),
+            "y": d3.mean(dataset, function (d,i) {
+                y = d.getAttribute('cy');
+                cluster = clusterC[i];
 
-  d3.select("#xLabel").text(dataName(v1));
-  d3.select("#yLabel").text(dataName(v2));
-  d3.select("#colorLabel").text(dataName(v3));
+                if (cluster == j) {
+                    return parseInt(y, 10);
+                }
+            }),
+            "cluster": j
+        }
+        if (centroidArr.length >= clusterNum && c.x == centroidArr[j - 1].x && c.y == centroidArr[j - 1].y) {
+            notMove++;
+        }
+        if (c.x > 0) {
+            centroidArr[j - 1] = c;
+        }        
+    }
 
-  d3.select("#xAxis").call(xAxis);
-  d3.select("#yAxis").call(yAxis);
+    if (notMove >= clusterNum) {
+        finish = true;
+    }
 
-  points.transition()
-    .duration(750)
-    .ease("cubic")
-    .attr("cx", function(d) { return x(d[v1]); })
-    .attr("cy", function(d) { return y(d[v2]); })
+    if (d3.selectAll('.centroid')[0].length > 0) {
+        svg.selectAll('.centroid')
+            .data(centroidArr)
+            .attr({
+                cx : function(d) { return d.x; },
+                cy : function(d) { return d.y; }
+            });
+    } else {
+        svg.selectAll('rect')
+            .data(centroidArr)
+            .enter()
+            .append('circle')
+            .attr({
+                cx : function(d){ return d.x; },
+                cy : function(d){ return d.y; },
+                r : 0,
+                'class': function(d,i){ return 'centroid cluster' + d.cluster; },
+                // height : function(d){ return d; },
+                fill : function(d,i){ return centroidColor[d.cluster]; },
+                "cluster" : function(d){ return d.cluster; }
+            })
+            .attr({
+                r: 7,
+                stroke: "black" 
+            });
+    }
+    centroid = false;
+	
+	if(!finish)
+		calDistance();
+}
+
+function calDistance() {
+	d3.selectAll('.dot')
+		.attr({
+		   fill: function (d,i) {
+				min = 10000000;
+				cluster = 0;
+				for (var j = 1; j <= clusterNum; j++) {
+					distance = 
+						Math.sqrt(
+							Math.pow((centroidArr[j - 1].x - xFunc(d.tsneX)), 2) + Math.pow((centroidArr[j - 1].y - yFunc(d.tsneY)), 2)
+						);
+
+					if (min >= distance) {
+						min = distance;
+						cluster = j;
+					}
+				}
+				clusterC[i] = cluster;
+				return clusterColor[cluster];
+		   }
+		});
+    centroid = true;
+	if(!finish)
+		calCentroid();
 }
 
 
